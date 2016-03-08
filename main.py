45950792f302b0549900fb11949bddb9a8edfc67
sreejith-mq <sreejith@mquotient.net>
2016-03-08 16:57:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,13 @@
 class MainOne(object):
     def __init__(self, name):
         self.name = name
-    
+
     def __str__(self):
         return "MainOne - {}".format(self.name)
-    
+
     def __unicode__(self):
         return unicode(self.__str__())
-        
+    
 if __name__=='__main__':
     main = MainOne('sreejith')
     print str(main)
