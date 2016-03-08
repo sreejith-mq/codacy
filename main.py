7318f5265421854cc18b18c5bf0398f7dfa49a34
sreejith-mq <sreejith@mquotient.net>
2016-03-08 16:42:18 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+# codacy
+
+
+class MainOne(object):
+    def __init__(self, name):
+        self.name = name
+    
+    def __str__(self):
+        return "MainOne - {}".format(self.name)
+    
+    def __unicode__(self):
+        return unicode(self.__str__())
+        
+if __name__=='__main__':
+    main = MainOne('sreejith')
+    print str(main)
